9ae27b3d189844faf665077ef6632d62e964693f
Kuba429 <jakubzawadzki429@gmail.com>
2021-08-21 13:22:38 +0200

    moved login router functions into middleware and fixed some issues

--- server/src/UserController.ts
+++ server/src/UserController.ts
@@ -7,39 +7,80 @@ class UserController {
 
     register(req: any, res: any, next: any) {
         req.body.outMessage = "Fine";
-    req.body.outStatus = "OK";
-    const saltRound = parseInt(<string>process.env.SALTROUNDS);
-    const { username, password } = req.body;
+        req.body.outStatus = "OK";
+        const saltRound = parseInt(<string>process.env.SALTROUNDS);
+        const { username, password } = req.body;
 
-    bcrypt.hash(password, saltRound, function (err, hash) {
-        if (err) {
-            req.body.outMessage = "ERROR";
-            req.body.outStatus = "Not OK";
-            res.json({
-                message: req.body.outMessage,
-                status: req.body.outStatus,
-            });
-        }
+        bcrypt.hash(password, saltRound, function (err, hash) {
+            if (err) {
+                req.body.outMessage = "ERROR";
+                req.body.outStatus = "Not OK, MARK 1";
 
-        pool.query(
-            "INSERT INTO users(username,password, created_on)\
+                next();
+            } else {
+                pool.query(
+                    "INSERT INTO users(username,password, created_on)\
              VALUES($1,$2, CURRENT_TIMESTAMP)",
-            [username, hash]
-        )
+                    [username, hash]
+                )
+                    .then((response) => {
+                        //SUCCESS BLOCK
+                        req.body.outMessage = "User Created";
+                    })
+                    .catch((err) => {
+                        req.body.outStatus = "Not OK";
+                        req.body.outMessage = err;
+                    })
+                    .finally(() => {
+                        next();
+                    });
+            }
+        });
+    }
+
+    login(req: any, res: any, next: any) {
+        req.body.outMessage = "Everything ok";
+        req.body.outStatus = "OK";
+        //get data from req body
+        const { username } = req.body;
+
+        //get hashed password from db
+
+        pool.query("SELECT * FROM users WHERE username LIKE $1;", [username])
+            //good query
             .then((response) => {
-                req.body.outMessage = response;
+                const userRow: any = response.rows[0];
+                const hashedPassword = userRow.password;
+                const givenPassword = req.body.password;
+
+                bcrypt
+                    .compare(givenPassword, hashedPassword)
+                    .then((result) => {
+                        if (result === true) {
+                            //SUCCESS BLOCK
+                            // correct password; give token
+                            req.body.outMessage = "Good password, logged in";
+                        } else {
+                            //incorrect password
+                            req.body.outMessage = "Incorrect password";
+                            req.body.outStatus = "Not ok";
+                        }
+                    })
+                    .catch((err) => {
+                        req.body.outStatus = "Not OK1";
+                        console.log(err);
+                        req.body.outMessage = err;
+                    })
+                    .finally(() => {
+                        next();
+                    });
             })
+            //bad query
             .catch((err) => {
                 req.body.outStatus = "Not OK";
-                req.body.outMessage = err;
-            })
-            .finally(() => {
-                res.json({
-                    status: req.body.outStatus,
-                    message: req.body.outMessage,
-                });
+                req.body.outMessage = "Wrong username";
+                next();
             });
-    });
     }
 }
 
--- server/src/routes/login.ts
+++ server/src/routes/login.ts
@@ -5,54 +5,12 @@ import pool from "../db";
 import user from "../UserController";
 export const router = express.Router();
 
+router.use('/', user.login)
 router.get("/", (req, res) => {
-    req.body.outMessage = "Everything ok";
-    req.body.outStatus = "OK";
-    //get data from req body
-    const { username } = req.body;
-
-    //get hashed password from db
-
-    pool.query("SELECT * FROM users WHERE username LIKE $1;", [username])
-        //good query
-        .then((response) => {
-            const userRow: any = response.rows[0];
-            const hashedPassword = userRow.password;
-            const givenPassword = req.body.password;
-
-            bcrypt
-                .compare(givenPassword, hashedPassword)
-                .then((result) => {
-                    if (result === true) {
-                        // correct password; give token
-                        req.body.outMessage = "Good password, logged in";
-                    } else {
-                        //incorrect password
-                        req.body.outMessage = "Incorrect password or username";
-                        req.body.outStatus = "Not ok";
-                    }
-                })
-                .catch((err) => {
-                    req.body.outStatus = "Not OK1";
-                    console.log(err);
-                    req.body.outMessage = err;
-                })
-                .finally(() => {
-                    res.json({
-                        status: req.body.outStatus,
-                        message: req.body.outMessage,
-                    });
-                });
-        })
-        //bad query
-        .catch((err) => {
-            req.body.outStatus = "Not OK2";
-            req.body.outMessage = err;
-            res.json({
-                status: req.body.outStatus,
-                message: req.body.outMessage,
-            });
-        });
+    res.json({
+        status: req.body.outStatus,
+        message: req.body.outMessage,
+    });
 });
 
 
