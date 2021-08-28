6d89279a7bb50f8ff79f70f7788112fdcfa7686e
Kuba429 <jakubzawadzki429@gmail.com>
2021-08-28 14:24:23 +0200

    adding tags to post inside form

--- client/src/components/PostForm.tsx
+++ client/src/components/PostForm.tsx
@@ -1,8 +1,37 @@
-import React from "react";
-
+import React, { useState, useRef, useEffect } from "react";
+import { v4 } from "uuid";
 export default function PostForm() {
+    const [tags, setTags] = useState([""]);
+    const [activeButton, setActiveButton] = useState(false);
+    const tagInput = useRef(null);
+
+    const addTag = () => {
+        const tagInputElement: any = tagInput.current;
+        let tagValue: string = tagInputElement.value;
+        //Return if empty
+        if (!tagValue) {
+            return;
+        }
+        const indexOfHash = tagValue.indexOf("#");
+        //Return if # inside
+        if (indexOfHash > 0) {
+            alert("Your tag can't contain '#' symbol");
+            return;
+            //remove # at the beginning
+        } else if (indexOfHash == 0) {
+            tagValue = tagValue.substring(1);
+        }
+        //set state and empty the input
+        setTags([...tags, tagValue]);
+        tagInputElement.value = "";
+    };
+
+    useEffect(() => {
+        setTags([]);
+    }, []);
+
     return (
-        <div className=" p-4 rounded-box form-control  flex flex-col gap-4">
+        <div className="p-4 rounded-box form-control flex flex-col gap-4">
             <input
                 placeholder="Title"
                 type="text"
@@ -20,16 +49,37 @@ export default function PostForm() {
                         type="text"
                         className="input input-primary w-4/5"
                         placeholder="tags"
+                        onInput={(e) => {
+                            //@ts-ignore
+                            e.target.value.length < 1
+                                ? setActiveButton(false)
+                                : setActiveButton(true);
+                        }}
+                        ref={tagInput}
                     />
-                    <button className="btn btn-primary w-1/5 text-base-200 text-sm lg:text-base">
+                    <button
+                        onClick={addTag}
+                        className={`${
+                            activeButton
+                                ? "btn  btn-accent text-base-200"
+                                : "btn-disabled"
+                        } btn-circle w-1/5 text-sm lg:text-base`}
+                    >
                         Add tag
                     </button>
                 </div>
                 <div className="container flex gap-4 flex-wrap">
                     {/* Tag prototype */}
-                    <div className="badge badge-primary text-base-200 cursor-pointer">
-                        Tag
-                    </div>
+                    {tags.map((item) => {
+                        return (
+                            <div
+                                key={v4()}
+                                className="badge badge-primary text-base-200 cursor-pointer"
+                            >
+                                {`#${item}`}
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
 
