60336dcc4fc49713ca059466c3fa348d605c9c4c
Kuba429 <jakubzawadzki429@gmail.com>
2021-08-28 21:32:17 +0200

    client getting posts from server

--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { stringify, v4 } from "uuid";
+import { v4 } from "uuid";
 
-export default function Post() {
-    const author = "Author";
-    const content = "This is a post. LOL";
-    const tags = ["funny", "viral", "video"];
-    let date = "20.03.2021";
+export default function Post({ author, content, tags, createdOn }) {
+    createdOn = new Date(createdOn).toDateString();
 
     return (
         <div className="rounded-box mt-8 p-4 bg-base-200 shadow">
             <div className="container flex items-end gap-2">
                 <h1 className="text-2xl leading-none">{author}</h1>
-                <p className="text-2xs text-base-content opacity-60">{date}</p>
+                <p className="text-2xs text-base-content opacity-60">
+                    {createdOn}
+                </p>
             </div>
             <p className="text-base py-2">{content}</p>
             {/* tags */}
--- client/src/components/PostContainer.tsx
+++ client/src/components/PostContainer.tsx
@@ -1,21 +1,59 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { v4 } from "uuid";
 import Post from "./Post";
 import PostFormWrapper from "./PostFormWrapper";
 
 export default function PostContainer() {
-    const [post, setPosts] = useState([]);
+    const [posts, setPosts] = useState(false);
+    const getPosts = async () => {
+        try {
+            const response = await axios({
+                url: "/posts/get",
+                method: "post",
+                baseURL: "http://localhost:5000",
+                headers: { "Content-Type": "application/json" },
+
+                data: {
+                    page: 1,
+                    howMany: 10,
+                },
+            });
+            setPosts(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     useEffect(() => {
-        console.log("eloelo320");
+        getPosts();
     }, []);
 
     return (
         <div className="flex flex-col w-11/12 lg:w-2/3">
             <PostFormWrapper />
+            <div
+                className="btn"
+                onClick={() => {
+                    console.log(posts);
+                }}
+            >
+                Click
+            </div>
 
-            <Post />
-            <Post />
+            {posts &&
+                //@ts-ignore
+                posts.map((post) => {
+                    return (
+                        <Post
+                            content={post.content}
+                            key={post.id}
+                            author={post.author}
+                            tags={post.tags}
+                            createdOn={post.created_on}
+                        />
+                    );
+                })}
         </div>
     );
 }
