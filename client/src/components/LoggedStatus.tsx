db89237e64012bd544ab468f672c4332a41a3300
Kuba429 <jakubzawadzki429@gmail.com>
2021-08-30 12:53:59 +0200

    LoggedStatus component

--- client/src/components/LoggedStatus.tsx
+++ client/src/components/LoggedStatus.tsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function LoggedStatus() {
-    return (
-        <li className="">
-            {/* a tag for now in order to keep consistent styling */}
-            <a>Login / Register</a>
-        </li>
+    const [isLogged, setIsLogged] = useState(true);
 
-      
-    );
+    //separate components not needed for now
+    if (isLogged) {
+        // logged
+        return (
+            <div className="dropdown dropdown-top w-auto">
+                <div tabIndex={0} className="m-1 btn btn-ghost ">
+                    Username
+                </div>
+                <ul
+                    tabIndex={0}
+                    className="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-auto "
+                    // without paddingInline styling li tags are beigh pushed to right
+                    style={{ paddingInlineStart: 10, paddingInlineEnd: 10 }}
+                >
+                    <li>
+                        <a>Log Out</a>
+                    </li>
+                    <li>
+                        <a>Profile</a>
+                    </li>
+                </ul>
+            </div>
+        );
+    } else {
+        // not logged
+        return (
+            <button className='btn btn-accent'>Sign In</button>
+        );
+    }
 }
