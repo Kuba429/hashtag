712326ec0173d4b4fb460511ba42222520d3a72a
Kuba429 <jakubzawadzki429@gmail.com>
2021-09-10 14:42:39 +0200

    minor spacing fix

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import PostContainer from "../components/PostContainer";
 export default function Home() {
     return (
         <div className="container flex flex-col justify-center items-center">
-            <h1 className="w-full pl-20 lg:w-2/3 text-5xl lg:pl-0">Home</h1>
+            <h1 className="w-full pl-20 mt-20 mb-5  lg:w-2/3 text-5xl lg:pl-0">Home</h1>
 
             <PostContainer />
         </div>
