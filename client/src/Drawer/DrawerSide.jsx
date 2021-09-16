35f06a18475482a5411dcdc0678be2d9bd074bfa
Kuba429 <jakubzawadzki429@gmail.com>
2021-09-16 19:12:31 +0200

    sidebar closes when changing location

--- client/src/Drawer/DrawerSide.jsx
+++ client/src/Drawer/DrawerSide.jsx
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import LoggedStatus from "../components/LoggedStatus";
 import SlideLinks from "../components/SlideLinks";
 
 export default function DrawerSide() {
+    const sidebarToggleRef = useRef(null);
+
     return (
         <div className="drawer-side shadow-lg">
             <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
 
             <ul className="menu flex justify-center align-middle p-4 overflow-y-auto w-80 text-base-content bg-base-200 md:bg-base-100">
                 <label
+                    ref={sidebarToggleRef}
                     htmlFor="my-drawer-2"
                     className="mb-4 w-full bg-base-200 text-gray-600 border-0 drawer-button flex justify-end lg:hidden"
                 >
-                    {/* @ts-ignore */}
-                    <ion-icon size="large" name="chevron-back-outline">
-                        {/* @ts-ignore */}
-                    </ion-icon>
+                    <ion-icon
+                        size="large"
+                        name="chevron-back-outline"
+                    ></ion-icon>
                 </label>
 
                 {/* Links */}
-                <SlideLinks />
+                <SlideLinks sidebarToggleRef={sidebarToggleRef} />
                 <LoggedStatus />
             </ul>
         </div>
--- client/src/components/SlideLinks.jsx
+++ client/src/components/SlideLinks.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { v4 } from "uuid";
 
-export default function SlideLinks() {
+export default function SlideLinks({ sidebarToggleRef }) {
+    const temp = ["123", "ddd"];
+    const hideSidebar = () => {
+        sidebarToggleRef.current.click();
+    };
     return (
         <div className="flex flex-col justify-center h-full">
-            <li className="py-2">
+            <li onClick={hideSidebar} className="py-2">
                 <Link to="/">Home</Link>
             </li>
-            <li className="py-2">
-                <Link to="/tag/elo">Tag</Link>
-            </li>
+            <hr />
+            {temp.map((item) => {
+                return (
+                    <li onClick={hideSidebar} key={v4()} className="py-2">
+                        <Link to={`/tag/${item}`}>{`#${item}`}</Link>
+                    </li>
+                );
+            })}
         </div>
     );
 }
